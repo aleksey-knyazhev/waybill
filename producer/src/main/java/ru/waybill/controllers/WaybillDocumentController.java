3d5b06073b6bff58a6d60381964ea05aa421891d
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2026-05-19 23:02:55 +0300

    Frontend

--- producer/src/main/java/ru/waybill/controllers/WaybillDocumentController.java
+++ producer/src/main/java/ru/waybill/controllers/WaybillDocumentController.java
@@ -3,12 +3,11 @@ package ru.waybill.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.waybill.models.WaybillDocument;
-import ru.waybill.helpers.WaybillDocumentExamples;
 
 @RestController
 public class WaybillDocumentController {
     @GetMapping("/api/waybill/example")
     public WaybillDocument getExampleDocument() {
-        return WaybillDocumentExamples.createExampleDocument();
+        return new WaybillDocument();
     }
 }
